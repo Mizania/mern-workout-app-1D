ffb278a4ef1fe144a76064a3d73536e75e1349e4
Clarence <23050214@myrp.edu.sg>
2025-02-03 19:40:29 +0800

    update

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,27 +1,32 @@
-import express from 'express';
-import {
+const express = require("express");
+const {
   getWorkouts,
   getWorkout,
   createWorkout,
   deleteWorkout,
   updateWorkout,
-} from '../controllers/workoutController.js';
+} = require("../controllers/workoutController");
 
+const requireAuth = require("../middleware/requireAuth");
+
+//make authentication before getting other routers
 const router = express.Router();
 
-// GET all workouts
-router.get('/', getWorkouts);
+router.use(requireAuth);
 
-// GET a single workout
-router.get('/:id', getWorkout);
+// Get all workouts
+router.get("/", getWorkouts);
 
-// POST a new workout
-router.post('/', createWorkout);
+// Get a single workout
+router.get("/:id", getWorkout);
 
-// DELETE a workout
-router.delete('/:id', deleteWorkout);
+// Post a new workout
+router.post("/", createWorkout);
 
-// UPDATE a workout
-router.patch('/:id', updateWorkout);
+// Delete a workout
+router.delete("/:id", deleteWorkout);
 
-export default router;
+// Update a workout
+router.patch("/:id", updateWorkout);
+
+module.exports = router;
